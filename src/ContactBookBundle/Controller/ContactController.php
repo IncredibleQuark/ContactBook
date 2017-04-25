a45515547f42d5926633a271d402f554a4d933b8
IncredibleQuark <lkupinski@gmail.com>
2017-04-25 22:52:46 +0200

    Added createNewContact action

--- src/ContactBookBundle/Controller/ContactController.php
+++ src/ContactBookBundle/Controller/ContactController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
@@ -16,12 +17,37 @@ class ContactController extends Controller
      * @Template(":Contact:new.html.twig")
      * @Method("GET")
      */
-    public function createNewContactAction()
+    public function createNewFormAction()
     {
         $contact = new Contact();
 
         $form = $this->createForm(ContactType::class, $contact,
                 ['action' => $this->generateUrl('contactbook_contact_createnewcontact')]);
+
+        return ['form' => $form->createView()];
+    }
+
+    /**
+     * @Route("/new")
+     * @Template(":Contact:new.html.twig")
+     * @Method("POST")
+     */
+    public function createNewContactAction(Request $request)
+    {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->redirectToRoute('contactbook_contact_createnewcontact');
+        }
 
         return ['form' => $form->createView()];
     }
