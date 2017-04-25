492ba5d6506605bb5c368a2afec949442f65c1f6
IncredibleQuark <lkupinski@gmail.com>
2017-04-25 22:38:23 +0200

    Created formBuilder, updated createNewContact action.

--- src/ContactBookBundle/Controller/ContactController.php
+++ src/ContactBookBundle/Controller/ContactController.php
@@ -3,6 +3,8 @@
 namespace ContactBookBundle\Controller;
 
 use ContactBookBundle\Entity\Contact;
+use ContactBookBundle\Form\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,15 +14,14 @@ class ContactController extends Controller
     /**
      * @Route("/new")
      * @Template(":Contact:new.html.twig")
+     * @Method("GET")
      */
     public function createNewContactAction()
     {
-        $post = new Contact();
-        $form = $this->createFormBuilder($post)
-            ->add('name', 'text')
-            ->add('surname', 'text')
-            ->add('description', 'text')
-            ->add('save', 'submit', array('label' => 'Send'))->getForm();
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact,
+                ['action' => $this->generateUrl('contactbook_contact_createnewcontact')]);
 
         return ['form' => $form->createView()];
     }
