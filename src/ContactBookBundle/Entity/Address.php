ceaa1df88f6bcea1d71f12c4d5e083a9e9ba7a54
IncredibleQuark <lkupinski@gmail.com>
2017-04-30 19:33:02 +0200

    Created Address entity with many to one relation to Contact

---
+++ src/ContactBookBundle/Entity/Address.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace ContactBookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="address")
+ * @ORM\Entity(repositoryClass="ContactBookBundle\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=50, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=50, nullable=true)
+     */
+    private $street;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="house_number", type="string", length=25, nullable=true)
+     */
+    private $houseNumber;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="apartment_number", type="string", length=25, nullable=true)
+     */
+    private $apartmentNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postal_code", type="string", length=25, nullable=true)
+     */
+    private $postalCode;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ContactBookBundle\Entity\Contact", inversedBy="address")
+     */
+    private $contact;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set houseNumber
+     *
+     * @param integer $houseNumber
+     * @return Address
+     */
+    public function setHouseNumber($houseNumber)
+    {
+        $this->houseNumber = $houseNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get houseNumber
+     *
+     * @return integer 
+     */
+    public function getHouseNumber()
+    {
+        return $this->houseNumber;
+    }
+
+    /**
+     * Set apartmentNumber
+     *
+     * @param integer $apartmentNumber
+     * @return Address
+     */
+    public function setApartmentNumber($apartmentNumber)
+    {
+        $this->apartmentNumber = $apartmentNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get apartmentNumber
+     *
+     * @return integer 
+     */
+    public function getApartmentNumber()
+    {
+        return $this->apartmentNumber;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param string $postalCode
+     * @return Address
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return string 
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set contact
+     *
+     * @param \ContactBookBundle\Entity\Contact $contact
+     * @return Address
+     */
+    public function setContact(\ContactBookBundle\Entity\Contact $contact = null)
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    /**
+     * Get contact
+     *
+     * @return \ContactBookBundle\Entity\Contact 
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+}
